cf97c6ac9ba7624a35d3c17ca5b575b47e3533de
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 16:00:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='2.1.0',
     packages=find_packages(),
     url='https://github.com/bessonovaleksey/crimeserieslinkage.git',
-    license='MIT',
+    license='Apache Software License',
     author='Aleksey A. Bessonov',
     author_email='bestallv@mail.ru',
     description='Statistical methods for identifying serial crimes and related offenders',
