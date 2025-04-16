df02e69f78c8bbe9b6095e7843deaa5e8f1ed595
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 13:12:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='crimeserieslinkage',
-    version='2.0.0',
+    version='2.1.0',
     packages=find_packages(),
     url='https://github.com/bessonovaleksey/crimeserieslinkage.git',
     license='MIT',
