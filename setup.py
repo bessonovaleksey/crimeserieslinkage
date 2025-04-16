6f9893063bc3f835a3dc78d410c09c3b6c7189c0
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 11:42:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='crimeserieslinkage',
     version='2.0',
     packages=find_packages(),
     url='https://github.com/bessonovaleksey/crimeserieslinkage.git',
+    packages=['crimeserieslinkage'],
     license='MIT',
     author='Aleksey A. Bessonov',
     author_email='bestallv@mail.ru',
     description='Statistical methods for identifying serial crimes and related offenders',
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache-2.0 License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
-        'numpy',
-        'pandas',
+        'numpy>=1.22.1',
+        'pandas>=1.5.1',
         'tqdm',
-        'scipy',
-        'sklearn',
-        'igraph',
-        'matplotlib',
+        'scipy>=1.7.3',
+        'sklearn>=1.3.0',
+        'igraph>=0.10.2',
+        'matplotlib>=3.5.1',
         'datetime'
     ],
 )
