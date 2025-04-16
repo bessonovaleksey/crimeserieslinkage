b11ca5eaab8e8810695c727120a4fd38624422f8
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 13:08:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='crimeserieslinkage',
-    version='2.0',
+    version='2.0.0',
     packages=find_packages(),
     url='https://github.com/bessonovaleksey/crimeserieslinkage.git',
     license='MIT',
@@ -20,13 +20,13 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'numpy>=1.22.1',
-        'pandas>=1.5.1',
+        'numpy',
+        'pandas',
         'tqdm',
-        'scipy>=1.7.3',
-        'sklearn>=1.3.0',
-        'igraph>=0.10.2',
-        'matplotlib>=3.5.1',
+        'scipy',
+        'sklearn',
+        'igraph',
+        'matplotlib',
         'datetime'
     ],
 )
