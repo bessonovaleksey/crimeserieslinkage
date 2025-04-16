ea9480e5117a39e5d0e4fd9e726b89da25318d13
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 19:08:37 +0300

    Update crimeserieslinkage.py

--- crimeserieslinkage:/crimeserieslinkage.py
+++ crimeserieslinkage:/crimeserieslinkage.py
@@ -70,19 +70,6 @@ seriesOffenderID(crime, unsolved, solved, seriesData, varlist, estimateBF,
 
 """
 from __future__ import division, absolute_import, print_function
-
-__all__ = ['bayes_prob', 'bayesPairs', 'cat_levels', 'clusterPath', 'compare_categorical',
-           'compare_numeric', 'compare_spatial', 'compareСrimes', 'comparetemporal',
-           'comparisonCrime', 'crimeClust_bayes', 'crimeClust_Hier', 'crimeCount', 
-           'crimeLink', 'crimeLink_Clust_Hier', 'datapreprocess', 'difftime', 'expabsdiff', 
-           'expabsdiff_circ', 'GBC', 'get_d', 'getBF', 'getCrimes', 'getCrimeSeries', 
-           'getCriminals', 'getROC', 'graphDataFrame', 'haversine', 'linkage_sID', 'make_breaks', 
-           'make_groups', 'make_linked', 'make_unlinked', 'makePairs', 'makeSeriesData', 
-           'naiveBayes', 'naivebayesfit', 'plot_bf', 'plot_crimeClust_bayes', 'plot_hcc', 
-           'plotBF', 'plotHCL', 'plotnaiveBayes', 'plotROC', 'predict_bf', 'predictGB', 
-           'predictnaiveBayes', 'seq', 'seriesCrimeID', 'seriesOffenderID']
-
-
 import numpy as np
 import pandas as pd
 import math
