8a24900add993f4cddf9f147fa947c3405671fc8
Aleksey <147443182+bessonovaleksey@users.noreply.github.com>
2025-04-16 16:10:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache-2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
